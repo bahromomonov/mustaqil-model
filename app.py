ea25cf12cbe9b0ae32d2d89166e84669a421083d
Bahrom <bahrom77774444@gmail.com>
2022-09-16 23:19:49 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,19 @@
+from tokenize import _all_string_prefixes
+from fastai.vision.all import *
+import streamlit as st
+import pathlib
+import plotly.express as px
+temp=pathlib.PosixPath
+
+pathlib.PosixPath=pathlib.WindowsPath
+st.title('Buyumlarni klassifikatsiyalovchi model')
+file=st.file_uploader("Rasm yuklash:,type=['jpeg','jpg','svg','gif']")
+if file:
+    st.image(file)
+    img=PILImage.create(file)
+    model=load_learner("mustaqil_model.pkl")
+    pred,pred_id,probs=model.predict(img)
+    st.success(f"Bashorat:{pred}")
+    st.info(f"Ehtimollik:{probs[pred_id]*100}%")
+    fig=px.bar(x=probs*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
